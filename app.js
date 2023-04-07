879ec4ffb44e32fac9be594e7d61c043c00ab3e1
Peter Briers <peter.briers@gmail.com>
2023-04-07 16:10:48 +0200

    A Nicer UI For Pipodoro

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-/**
- * @param {ReturnType<import('./ui').ui>} ui 
- * @param {ReturnType<timer>} timer 
- */
-export function app(ui, timer) {
-  
-  ui.setTime(timer.seconds);
+export class App {
 
-  timer.onTick((time) => {
-    ui.setTime(time);
-  });
+  #ui
+  #timer
 
-  ui.onStart(() => timer.start());
-  // ui.onStop(() => timer.stop());
+  /**
+   * @param {import('./ui').UI} ui 
+   * @param {import('./timer').timer} timer 
+   */
+  constructor(ui, timer) {
+    ui.setTime(timer.seconds);
+    timer.onTick((time) => ui.setTime(time));
+
+    this.#timer = timer;
+    this.#ui = ui;
+  }
+
+  start() {
+    this.#ui.setStart();
+    this.#timer.start();
+  }
+
+  pause() {
+    this.#ui.setPause();
+    this.#timer.pause();
+  }
+
+  popout() {
+    this.#ui.popout();
+  }
 }
--- ui.js
+++ ui.js
@@ -1,54 +1,47 @@
-/**
- * @param {Object} obj - An object.
- * @param {HTMLButtonElement} obj.startButton .
- * @param {HTMLButtonElement} obj.settingsButton
- * @param {HTMLVideoElement} obj.video
- */
-export function ui({startButton, video}) {
-  const {canvas, canvasCtx} = setupCanvas();
-
-  /** @type {Function} */
-  let startCallback = () => null;
-
-  setupVideo();
-  setupButtons();
-
-  return {
-    setTime,
-    onStart,
-  }
+export class UI {
+  #painter
+  #player
+  #startButton
+  #pauseButton
 
   /**
-   * @param {Function} callback 
-   **/
-  function onStart(callback) {
-    startCallback = callback;
+   * @param {Object} parameters
+   * @param {HTMLVideoElement} parameters.video
+   * @param {HTMLButtonElement} parameters.startButton
+   * @param {HTMLButtonElement} parameters.pauseButton
+   * @param {HTMLButtonElement} parameters.pipButton
+   */
+  constructor({video, startButton, pauseButton, pipButton}) {
+    this.#painter = new Painter();
+    this.#player = new Player(this.#painter, video);
+    this.#startButton = startButton;
+    this.#pauseButton = pauseButton;
+    this.setup();
+  }
+
+  setup() {
+    this.#toggle(this.#pauseButton, false);
+  }
+
+  setStart() {
+    this.#toggle(this.#startButton, false);
+    this.#toggle(this.#pauseButton, true);
+  }
+
+  setPause() {
+    this.#toggle(this.#startButton, true);
+    this.#toggle(this.#pauseButton, false);
+  }
+
+  popout() {
+    this.#player.popout();
   }
 
   /** 
    * @param {number} seconds 
    **/
-  function setTime(seconds) { 
-    writeToCanvas(formatTime(seconds));
-  }
-
-  function setupCanvas() {
-    const canvas = document.createElement('canvas');
-    canvas.style.display = 'none';
-    const canvasCtx = canvas.getContext('2d');
-    document.body.appendChild(canvas);
-    return {canvas, canvasCtx};
-  }
-
-  function setupVideo() {
-    video.muted = true;
-    video.controls = true;
-    video.srcObject = canvas.captureStream();
-    video.play();
-  }
-
-  function setupButtons() {
-    startButton.addEventListener('click', () => startCallback());
+  setTime(seconds) {
+    this.#painter.paint(this.#formatTime(seconds));
   }
 
   /**
@@ -56,23 +49,76 @@ export function ui({startButton, video}) {
    * @param {Number} seconds 
    * @returns {String} MM:SS
    */
-   function formatTime(seconds) {
+  #formatTime(seconds) {
     return new Date(seconds * 1000).toISOString().substring(14, 19)
+  }
+
+  #toggle(button, show) {
+    button.style.display = show ? 'block' : 'none';
+  }
+}
+
+class Player {
+
+  #video
+  #isPip = false;
+
+  /**
+   * @param {HTMLVideoElement} video
+   * @param {Painter} painter
+   */
+  constructor(painter, video) {
+    video.muted = true;
+    video.controls = false;
+    video.srcObject = painter.stream;
+    video.play();
+    this.#video = video;
+  }
+
+  popout() {
+    if(this.#isPip) {
+      document.exitPictureInPicture();
+    } else {
+      this.#video.requestPictureInPicture();
+    }
+    this.#isPip = !this.#isPip;
+  }
+}
+
+class Painter {
+
+  #canvas
+  #ctx
+  stream;
+
+  constructor() {
+    const canvas = document.createElement('canvas');
+
+    canvas.width = 3840;
+    canvas.height = 2160;
+    
+    canvas.style.display = 'none';
+    
+    this.#canvas = canvas;
+    this.#ctx = canvas.getContext('2d');
+    this.stream = canvas.captureStream();
   }
 
   /**
    * 
    * @param {string} text 
-   * @returns 
+   * @returns void
    */
-  function writeToCanvas(text) {
-    if(!canvasCtx) return;
-    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-    canvasCtx.fillStyle = "#FFFFFF";
-    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-    canvasCtx.fillStyle = "#59FFA0";
-    canvasCtx.font = '52px sans-serif';
-    canvasCtx.textAlign = 'center';
-    canvasCtx.fillText(text, canvas.width / 2, canvas.height / 2);
+  paint(text) {
+    const ctx = this.#ctx;
+    const cv = this.#canvas;
+
+    ctx.clearRect(0, 0, cv.width, cv.height);
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, cv.width, cv.height);
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = '1000px sans-serif';
+    ctx.textAlign = 'center';
+    ctx.fillText(text, cv.width / 2, cv.height / 2 + ctx.measureText(text).actualBoundingBoxAscent / 2);
   }
 }
